fe31462607152b5527e6607987286664b7f43314
eslami <ab.eslami2001@gmail.com>
2024-08-12 15:19:32 +0330

    feat: migrate

---
+++ app/migration/migrate.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/eslami200117/clientCli/app/entities"
+	"github.com/eslami200117/clientCli/config"
+	"github.com/eslami200117/clientCli/database"
+)
+
+func main()	{
+	conf := config.GetConfig()
+	db := database.NewPostgresDatabase(conf)
+	authMigrate(db)
+}
+
+func authMigrate(db *database.PostgresDatabase) {
+	db.GetDb().Migrator().CreateTable(&entities.AuthEntity{})
+
+}
