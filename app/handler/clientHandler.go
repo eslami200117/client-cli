c3b5c1a8948ae448075ac61a2417f63112745629
eslami <ab.eslami2001@gmail.com>
2024-08-13 09:19:08 +0330

    feat: add weather data model

--- app/handler/clientHandler.go
+++ app/handler/clientHandler.go
@@ -6,17 +6,20 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/eslami200117/clientCli/app/repository"
 	"github.com/eslami200117/clientCli/config"
 	"github.com/eslami200117/clientCli/database"
+	"github.com/eslami200117/clientCli/app/models"
 )
 
 type auth struct {
 	Token string
 }
 
-var requestURL string = "http://localhost:5000"
+const requestURL = "http://localhost:5000"
+const receiveTime = time.Second
 
 
 func LoginHandler(username string, password string) {
@@ -71,18 +74,12 @@ func ListHandler(username string) {
 		fmt.Println("Failed to create marshal username", err)
 		return
 	}
-
+	token := getTokenByUser(username)
 	req, err := http.NewRequest(http.MethodGet, requestURL+"/test/list", bodyReader)
 	if err != nil {
 		fmt.Println("Failed to create request", err)
 		return
 	}
-
-	config := config.GetConfig()
-	db := database.NewPostgresDatabase(config)
-	repo := repository.NewRepo(db)
-	var token string = repo.GetToken(username)
-
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer "+token)
 
@@ -112,5 +109,48 @@ func ListHandler(username string) {
 
 
 func NodeHandler(username string) {
+	jsonData, err := json.Marshal(username)
+	bodyReader := bytes.NewReader(jsonData)
+	if err != nil {
+		fmt.Println("Failed to create marshal username", err)
+		return
+	}
+	token := getTokenByUser(username)
+	req, err := http.NewRequest(http.MethodGet, requestURL+"/test/node", bodyReader)
+	if err != nil {
+		fmt.Println("Failed to create request", err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	for {
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Println("client: error making http request:", err)
+			return
+		}
+
+		resBody, err := io.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println("client: could not read response body:", err)
+			return
+		}
+		var value model.Weather
+		err = json.Unmarshal(resBody, &value)
+		if err != nil{
+			fmt.Println("line 103 error in unmarshal", err)
+		}
+		fmt.Println(value)
+		time.Sleep(receiveTime)
+	}
+}
+
+func getTokenByUser(username string) string {
+	config := config.GetConfig()
+	db := database.NewPostgresDatabase(config)
+	repo := repository.NewRepo(db)
+	var token string = repo.GetToken(username)
+	return token
 	
 }
--- cmd/node.go
+++ cmd/node.go
@@ -27,6 +27,7 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(nodeCmd)
+	nodeCmd.PersistentFlags().StringVarP(&username, "username", "u", "", "username for get node")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
